81ca9a2218067e23e7e260bd51a5abb6f49945f7
mungaii <theforexer1@gmail.com>
2024-04-12 15:35:22 +0300

    Modfiying Illustartions 2: Promise.all() function

--- promise.js
+++ promise.js
@@ -58,7 +58,7 @@ const urls = [
     "https://jsonplaceholder.typicode.com/todos"
 ]
 
-Promise.all(urls.map((url) => {return fetch(url).then((response) => response.json())})).then((results) => {
+Promise.all(urls.map((url) => fetch(url).then((response) => response.json()))).then((results) => {
     console.log(results[0]);
     console.log(results[1]);
     console.log(results[2]);
