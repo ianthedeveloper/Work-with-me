4b2240ffbf8e9710d38d9e1070bc09254dbae8bf
mungaii <theforexer1@gmail.com>
2024-04-12 11:56:49 +0300

    Illustrating use: Promise.all()

--- promise.js
+++ promise.js
@@ -1,5 +1,5 @@
 const promise = new Promise ((resolve, reject) => {
-    if(true){
+    if(false){
         return resolve("It Worked!");
     }else{
         return reject("Error! Error! Error!");
@@ -14,10 +14,36 @@ const promise = new Promise ((resolve, reject) => {
 
 
 // 2
-    promise.then((result) => result + "!")
+    Promise.then((result) => result + "!")
     .then((result2) => result2 + "?")
     .then((result3) =>{
         throw Error;
         console.log(result3)
     })
     .catch((error) => console.log("Error!"))
+
+
+
+
+
+    // Promise.all()
+    const promise = new Promise ((resolve, reject) => {
+        if(false){
+            return resolve("It Worked!");
+        }else{
+            return reject("Error! Error! Error!");
+        }
+    })
+
+    
+    const promise2 = new Promise ((resolve, reject) => setTimeout(resolve, 100, "Tom"))
+
+    
+    const promise3 = new Promise ((resolve, reject) => setTimeout(resolve, 1000, "Dick"))
+
+
+    const promise4 = new Promise ((resolve, reject) => setTimeout(resolve, 2000, "Harry"))
+
+    promise.all([promise, promise2, promise3, promise4])
+            .then((results) => console.log(results))
+    
