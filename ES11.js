da11d9d34195ff457e8eedf1008e1ab9bfc4e362
mungaii <theforexer1@gmail.com>
2024-04-14 21:08:41 +0300

    Revising: 'Promise.all()'

--- ES11.js
+++ ES11.js
@@ -12,3 +12,64 @@ Promise.all([haha, ahhh])
 Promise.allSettled([haha, ahhh])
         .then((data) => console.log(data))
         .catch((error) => console.log("Error!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// REVISION
+const promise1 = new Promise ((reject, resolve) => 
+        resolve("The Future is bright. Never doubt that! ")
+); 
+
+const promise2 = new Promise ((resolve, reject) => {
+        return setTimeout(resolve,1000, "Accept your identity & make the most you can make out of it.")
+});
+
+const promise3 = new Promise((reject, resolve) => 
+        resolve("You gotta make this happen. YOu got no options left. Big Up G!")
+);
+
+Promise.all([promise1, promise2, promise2])
+
