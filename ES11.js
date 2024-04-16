18d86d57c9876628cf238bed84df50500306693f
mungaii <theforexer1@gmail.com>
2024-04-16 20:51:12 +0300

    Revision: Promise.allSettled()

--- ES11.js
+++ ES11.js
@@ -31,3 +31,24 @@ Promise.all([promise1, promise2, promise3])
         .then((results) => console.log(results) )
         .catch((error) => console.log("Error", error))
 
+
+
+
+
+
+// Promise.any()
+const promise1 = new Promise ((resolve, reject) => 
+        resolve("The Future is bright. Never doubt that! ")
+); 
+
+const promise2 = new Promise ((resolve, reject) =>{
+        return setTimeout(() => resolve("Thank You God For The Day!"), 1000)
+});
+
+const promise3 = new Promise((resolve, reject) => 
+        resolve("You gotta make this happen. You got no options left. Big Up G!")
+);
+
+Promise.any([promise1, promise2, promise3])
+        .then((results) => console.log(results) )
+        .catch((error) => console.log("Error", error)
