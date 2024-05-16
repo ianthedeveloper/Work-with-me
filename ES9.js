b8e9a19d72530d98c82744244bfa87464e313ef5
ianthedevveloper <thedeveloperian@gmail.com>
2024-05-16 18:11:54 +0300

    Modifications: Changed character names, ES9.js

--- ES9.js
+++ ES9.js
@@ -6,9 +6,9 @@
 
 // 1. Spreads in Objects
 const xchtrs = {
-    xchtr1: "Kim",
-    xchtr2: "Kennedy",
-    xchtr3: "Kimnerly"
+    xchtr1: "Jameson",
+    xchtr2: "Black Label",
+    xchtr3: "Ciroc"
 }
 
 const {xchtr2,xchtr3, ...rest} = xchtrs;
