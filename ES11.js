f866610f57c5c0b45ff689b0b0dbca235aedb55f
ianthedevveloper <thedeveloperian@gmail.com>
2024-05-23 08:47:11 +0300

    Modified resolve message, ES11.js

--- ES11.js
+++ ES11.js
@@ -48,7 +48,7 @@ const promise2 = new Promise ((resolve, reject) =>
 );
 
 const promise3 = new Promise((resolve, reject) =>
-        resolve("McLaren 765LT")
+        resolve("McLaren Sena")
 );
 
 Promise.any([promise1, promise2, promise3])
