4177e3b97d7ff0d5d32f37267664f41e97984569
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-06 09:58:45 +0300

    Modified resolve value, ES11.js

--- ES11.js
+++ ES11.js
@@ -48,7 +48,7 @@ const promise2 = new Promise ((resolve, reject) =>
 );
 
 const promise3 = new Promise((resolve, reject) =>
-        resolve("BUggati Chiron")
+        resolve("BUggati Chiron Super Sport")
 );
 
 Promise.any([promise1, promise2, promise3])
