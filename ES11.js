019e247f3e3b5a623c3e69ed6221a7eedefc6168
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-07 09:10:21 +0300

    Changed resolve message value, ES11.js

--- ES11.js
+++ ES11.js
@@ -44,7 +44,7 @@ const promise1 = new Promise ((resolve, reject) =>
 ); 
 
 const promise2 = new Promise ((resolve, reject) =>
-        resolve("BMW M6")
+        resolve("Bently Continental GTS")
 );
 
 const promise3 = new Promise((resolve, reject) =>
