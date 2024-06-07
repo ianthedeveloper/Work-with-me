9ea9ce7de12c528d95530c2144446d6ab041eba5
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-07 09:14:14 +0300

    Changed xchtr values, ES9.js

--- ES9.js
+++ ES9.js
@@ -7,8 +7,8 @@
 // 1. Spreads in Objects
 const xchtrs = {
     xchtr1: "This Too",
-    xchtr2: "More Keen",
-    xchtr3: "On What Happens Aroun You At All Times"
+    xchtr2: "Shall",
+    xchtr3: "Pass!"
 }
 
 const {xchtr2,xchtr3, ...rest} = xchtrs;
