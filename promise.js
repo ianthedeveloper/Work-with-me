cb4562ac0b1f5957a17d76c610d0c14284a6a731
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-11 11:54:40 +0300

    Modified resolve value, promise.js

--- promise.js
+++ promise.js
@@ -43,7 +43,7 @@ const promise = new Promise ((resolve, reject) => {
     const promise3 = new Promise ((resolve, reject) => setTimeout(resolve, 2000, "Maserrati"))
 
 
-    const promise4 = new Promise ((resolve, reject) => setTimeout(resolve, 5000, "2000 Edition, Chevy Camaro"))
+    const promise4 = new Promise ((resolve, reject) => setTimeout(resolve, 5000, "2000 Edition, Ford Mustang"))
 
     Promise.all([promise, promise2, promise3, promise4])
             .then((results) => console.log(results))
