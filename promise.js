55eb7dfbb44b9e99aa394b3dd1e35d25d3bdd53d
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-12 09:18:46 +0300

    Changed resolve value message contents, promise.js

--- promise.js
+++ promise.js
@@ -43,7 +43,7 @@ const promise = new Promise ((resolve, reject) => {
     const promise3 = new Promise ((resolve, reject) => setTimeout(resolve, 4000, "Maserrati"))
 
 
-    const promise4 = new Promise ((resolve, reject) => setTimeout(resolve, 6000, "2000 Edition, Ford Mustang"))
+    const promise4 = new Promise ((resolve, reject) => setTimeout(resolve, 6000, "Ferrari"))
 
     Promise.all([promise, promise2, promise3, promise4])
             .then((results) => console.log(results))
