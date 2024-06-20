0b61a583216169f76a06f763f57b73bdaa705764
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-20 14:24:00 +0300

    Changed Xchtr value, ES11.js

--- ES11.js
+++ ES11.js
@@ -48,7 +48,7 @@ const promise2 = new Promise ((resolve, reject) =>
 );
 
 const promise3 = new Promise((resolve, reject) =>
-        resolve("Bugatti Divo")
+        resolve("Rolls Royce Dawn")
 );
 
 Promise.any([promise1, promise2, promise3])
