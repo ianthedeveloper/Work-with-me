7f8b43ed5dc1b0c40840973c3d489576c92311e5
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-24 08:33:46 +0300

    Modified Resolve value, ES11.js

--- ES11.js
+++ ES11.js
@@ -48,7 +48,7 @@ const promise2 = new Promise ((resolve, reject) =>
 );
 
 const promise3 = new Promise((resolve, reject) =>
-        resolve("Rolls Royce Dawn")
+        resolve("Pagani, Limited Edition")
 );
 
 Promise.any([promise1, promise2, promise3])
