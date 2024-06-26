da595404906287580eab7788175389eb9fd5b6dc
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-26 09:11:22 +0300

    Modifications: Error message, ES11.js

--- ES11.js
+++ ES11.js
@@ -53,4 +53,4 @@ const promise3 = new Promise((resolve, reject) =>
 
 Promise.any([promise1, promise2, promise3])
         .then((results) => console.log(results) )
-        .catch((error) => console.log("Error! Error! Error!", error))
+        .catch((error) => console.log("Oops! There was an error", error))
