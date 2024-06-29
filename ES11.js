0c691f9340bdcb03f4a3ec0b16beac4630e224ae
ianthedevveloper <thedeveloperian@gmail.com>
2024-06-29 10:57:00 +0300

    Modified Resolve values, ES11.js

--- ES11.js
+++ ES11.js
@@ -44,7 +44,7 @@ const promise1 = new Promise ((resolve, reject) =>
 ); 
 
 const promise2 = new Promise ((resolve, reject) =>
-        resolve("Bently Continental GTS")
+        resolve("Aston Martin DBS")
 );
 
 const promise3 = new Promise((resolve, reject) =>
